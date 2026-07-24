8b7a97ef7d829ef9d22bae6c7b8ae51c8b4e1227
NgDanhThanhTrung <thanhtrungnguyendanh@gmail.com>
2026-07-24 18:07:05 +0700

    Update cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,5 +1,10 @@
 const cloudinary = require('cloudinary').v2;
 
+// Kiểm tra xem biến môi trường đã được cung cấp chưa
+if (!process.env.CLOUDINARY_CLOUD_NAME || !process.env.CLOUDINARY_API_KEY || !process.env.CLOUDINARY_API_SECRET) {
+  console.warn('⚠️ Cảnh báo: Thiếu cấu hình biến môi trường Cloudinary trong .env!');
+}
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
